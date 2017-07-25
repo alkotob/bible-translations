5aa95e15905b80e3c0023f434fd83759285fbeff
XJ <yahya@albanda.com>
2017-07-25 21:07:58 +0300

    Added some bible translations

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bible Translations
+
+These bible translations were provided by the authors with permission. Please see the individual translations authors for details on using in your own project.
---
+++ scripts/parse.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'rubygems/dependency_installer'
+require 'json'
+
+def require_gem gem_name
+  begin
+    gem gem_name
+  rescue LoadError
+    Gem::DependencyInstaller.new.install gem_name
+  end
+  require gem_name
+end
+
+require_gem 'nokogiri'
+
+output = { data: [] }
+
+dir = File.expand_path('../data', File.dirname(__FILE__))
+Dir.entries(dir).each do |filename|
+  next if filename.start_with? '.'
+  file = File.join(dir, filename)
+
+  doc = Nokogiri::XML(File.open(file))
+
+  # Parse metadata
+  meta = doc.css('bible').first
+  t_id = meta['id']
+  iso = meta['language']
+  direction = meta['direction']
+  original = t_id === 'greek'
+  code = "q#{meta['LanguageIsoCode']}#{t_id}"
+  code = "bible" if original
+  name = meta['name']
+
+  output[:data] << {
+    id: t_id,
+    code: code,
+    original: original,
+    name: name,
+    direction: direction,
+    language: iso,
+    file: filename
+  }
+end
+
+# Sort by translation ID
+output[:data] = output[:data].sort_by{|e| e[:id]}
+
+metadata_file = File.expand_path('../metadata.json', File.dirname(__FILE__))
+File.open(metadata_file, 'w') do |f|
+  f.write(JSON.pretty_generate(output))
+end
